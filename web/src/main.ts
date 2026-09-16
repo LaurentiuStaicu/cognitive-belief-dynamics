940f866a1f77741b2e92fc201c0d17397c236e97
LaurentiuStaicu <137863020+LaurentiuStaicu@users.noreply.github.com>
2026-09-16 20:40:58 +0300

    OA-7: load adaptive plan styles

--- web/src/main.ts
+++ web/src/main.ts
@@ -10,6 +10,7 @@ import './decision-uncertainty-ui.css';
 import './action-canvas.css';
 import './indicator-objects.css';
 import './signposts-triggers.css';
+import './adaptive-plan-runtime.css';
 import {chartSeries} from './visual-language';
 import {mountSemanticSearch} from './search-ui';
 import {mountSemanticInspector} from './inspector-ui';
